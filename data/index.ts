f6d61462035752924526de0fa0c0a1add371f846
silliansouhail <souhailsillian3@gmail.com>
2024-08-07 01:44:20 +0100

    soacial liks added

--- data/index.ts
+++ data/index.ts
@@ -211,13 +211,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/silliansouhail",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://x.com/LaihousWebdev",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/souhail-sillian-060242264/",
   },
 ];
